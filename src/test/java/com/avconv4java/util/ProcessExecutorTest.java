1076981b4a6a1b86968ecdc3bcc672588849bc73
Vladislav Bauer <bauer.vlad@gmail.com>
2014-02-04 21:46:11 +0700

    add unit tests for ProcessInfo class

--- src/test/java/com/avconv4java/util/ProcessExecutorTest.java
+++ src/test/java/com/avconv4java/util/ProcessExecutorTest.java
@@ -4,7 +4,6 @@ import com.avconv4java.util.process.ProcessExecutor;
 import com.avconv4java.util.process.ProcessInfo;
 import org.testng.Assert;
 import org.testng.SkipException;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -20,26 +19,41 @@ public class ProcessExecutorTest {
     private static final List<String> COMMAND_LS = Arrays.asList("ls");
     private static final List<String> COMMAND_SLEEP = Arrays.asList("sleep", "1");
 
+    private static final String MESSAGE_OS_TYPE = "Test is only for Unix OS";
 
-    @BeforeClass
-    public void init() {
-        if (!AVUtils.isUnix()) {
-            throw new SkipException("Test is only for Unix OS");
-        }
+
+    public void testNixCommandWithEnoughTimeout() throws Exception {
+        assumeOS();
+        Assert.assertTrue(runCommand(COMMAND_LS, 30000L, false).isSuccess());
+        Assert.assertTrue(runCommand(COMMAND_LS, 30000L, true).isSuccess());
     }
 
-
-    public void testNixCommandWithTimeout() throws Exception {
-        Assert.assertTrue(runCommand(COMMAND_LS, 30000L, false).isSuccess());
+    public void testNixCommandWithNotEnoughTimeout() throws Exception {
+        assumeOS();
         Assert.assertTrue(runCommand(COMMAND_SLEEP, 1L, false).isError());
-
-        Assert.assertTrue(runCommand(COMMAND_LS, 30000L, true).isSuccess());
         Assert.assertTrue(runCommand(COMMAND_SLEEP, 1L, true).isError());
     }
 
     public void testNixCommandWithoutTimeout() throws Exception {
+        assumeOS();
         Assert.assertTrue(runCommand(COMMAND_LS, null, false).isSuccess());
         Assert.assertTrue(runCommand(COMMAND_LS, null, true).isSuccess());
+    }
+
+    public void testErrorProcessInfo() {
+        final ProcessInfo info = ProcessInfo.error(MESSAGE_OS_TYPE);
+        Assert.assertTrue(info.isError());
+        Assert.assertFalse(info.isSuccess());
+        Assert.assertNull(info.getOutput());
+        Assert.assertEquals(info.getErrorOutput(), MESSAGE_OS_TYPE);
+    }
+
+    public void testCorrectProcessInfo() {
+        final ProcessInfo info = ProcessInfo.create(ProcessInfo.EXIT_CODE_SUCCESS, MESSAGE_OS_TYPE, null);
+        Assert.assertFalse(info.isError());
+        Assert.assertTrue(info.isSuccess());
+        Assert.assertNull(info.getErrorOutput());
+        Assert.assertEquals(info.getOutput(), MESSAGE_OS_TYPE);
     }
 
 
@@ -51,6 +65,12 @@ public class ProcessExecutorTest {
             Assert.assertTrue(statusCode != ProcessInfo.EXIT_CODE_SUCCESS);
         }
         return processInfo;
+    }
+
+    private void assumeOS() {
+        if (!AVUtils.isUnix()) {
+            throw new SkipException(MESSAGE_OS_TYPE);
+        }
     }
 
 }
