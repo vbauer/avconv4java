2261c3000ee8df0a5db2debfe85a9d6f3db81eba
Vladislav Bauer <bauer.vlad@gmail.com>
2014-02-06 18:48:48 +0700

    add "-benchmark" flag in advanced options

---
+++ src/main/java/com/avconv4java/option/advacned/AVAdvancedOptions.java
@@ -0,0 +1,39 @@
+package com.avconv4java.option.advacned;
+
+import com.avconv4java.core.AVOptions;
+
+/**
+ * @author Vladislav Bauer
+ */
+
+public class AVAdvancedOptions extends AVOptions {
+
+    public static final String FLAG_BENCHMARK = "-benchmark";
+
+
+    public static AVAdvancedOptions create() {
+        return new AVAdvancedOptions();
+    }
+
+
+    @Override
+    public AVAdvancedOptions flags(final Object... flags) {
+        return (AVAdvancedOptions) super.flags(flags);
+    }
+
+    @Override
+    public AVAdvancedOptions builders(final AVOptions... builders) {
+        return (AVAdvancedOptions) super.builders(builders);
+    }
+
+
+    /**
+     * ‘-benchmark (global)’
+     * Show benchmarking information at the end of an encode. Shows CPU time used and maximum memory consumption.
+     * Maximum memory consumption is not supported on all systems, it will usually display as 0 if not supported.
+     */
+    public AVAdvancedOptions benchmark() {
+        return flags(FLAG_BENCHMARK);
+    }
+
+}
